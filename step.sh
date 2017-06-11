cedf0583ae897abd8cd879cbb1ff47503d49bf57
rubentorresbonet <rubentorresbonet@gmail.com>
2017-06-11 15:22:35 +0200

    initial code

---
+++ step.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ "$(uname)" == "Darwin" ]; then
+    /Applications/Unity/Unity.app/Contents/MacOS/Unity -logfile -quit -batchmode \
+        -username $unity_email -password $unity_password -serial $unity_serial
+else
+    xvfb-run /opt/Unity/Editor/Unity -logfile -quit -batchmode -nographics -force-opengl \
+        -username $unity_email -password $unity_password -serial $unity_serial
+fi
