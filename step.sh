e0319d41898e63836a30de106a0c2f73850684c2
rubentorresbonet <rubentorresbonet@gmail.com>
2017-06-11 18:16:47 +0200

    removed debug

--- step.sh
+++ step.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -ex
-
 if [ "$(uname)" == "Darwin" ]; then
     /Applications/Unity/Unity.app/Contents/MacOS/Unity -logfile -quit -batchmode \
         -username $unity_email -password $unity_password -serial $unity_serial
