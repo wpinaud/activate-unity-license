d4ceff667b6fc2950814b103ce04404de2cf8009
rubentorresbonet <rubentorresbonet@gmail.com>
2017-06-11 18:25:44 +0200

    attempt to remove unity garbage

--- step.sh
+++ step.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-mkdir _tmp
-
 if [ "$(uname)" == "Darwin" ]; then
     /Applications/Unity/Unity.app/Contents/MacOS/Unity -logfile -quit -batchmode \
         -username $unity_email -password $unity_password -serial $unity_serial \
-        -projectPath _tmp
+        -projectPath /tmp
 else
     xvfb-run /opt/Unity/Editor/Unity -logfile -quit -batchmode -nographics -force-opengl \
         -username $unity_email -password $unity_password -serial $unity_serial \
-        -projectPath _tmp
+        -projectPath /tmp
 fi
-
-rm -rf _tmp
