cfe741bd618ffe6cc09590d4af910959fa8ac197
Willis Pinaud <willispinaud@gmail.com>
2017-10-29 15:11:37 -0400

    Update step.sh

--- step.sh
+++ step.sh
@@ -2,11 +2,6 @@
 
 mkdir ____tmp
 
-printenv
-echo $unity_email
-echo $unity_password 
-echo $unity_serial
-
 if [ "$(uname)" == "Darwin" ]; then
     /Applications/Unity/Unity.app/Contents/MacOS/Unity -logfile -quit -batchmode \
         -username $unity_email -password $unity_password -serial $unity_serial \
